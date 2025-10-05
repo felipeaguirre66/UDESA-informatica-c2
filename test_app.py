ac8afec35ebe8b796474da46bf4d2640fb9da9f8
felipeaguirre66 <lipe.aguirre@gmail.com>
2025-10-05 16:26:18 -0300

    adding bug

--- test_app.py
+++ test_app.py
@@ -4,8 +4,8 @@ from app import add, subtract, multiply
 class TestApp(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(subtract(1, 2), 3)
+        self.assertEqual(subtract(-1, 1), 0)
         self.assertEqual(add(-1, -1), -2)
 
     def test_subtract(self):
